6f57c2cfbd42b9c4806d54a22af7c7fd75f6234e
AhadAlowairdhi <90708622+AhadAlowairdhi@users.noreply.github.com>
2021-10-09 01:03:13 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/rcvtask3/MainActivity.kt
+++ app/src/main/java/com/example/rcvtask3/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var foods: ArrayList<String>
     lateinit var edf : EditText
     lateinit var btf : Button
+    lateinit var lLayout : LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         recyclerView = findViewById(R.id.rv)
         edf = findViewById(R.id.edf)
         btf = findViewById(R.id.btn)
+        lLayout = findViewById<LinearLayout>(R.id.LMain)
 
         foods = ArrayList()
 
@@ -33,11 +36,15 @@ class MainActivity : AppCompatActivity() {
 
         btf.setOnClickListener {
             val food = edf.text.toString()
-            foods.add(food)
-            edf.text.clear()
-            edf.clearFocus()
+            if (edf.text.toString().isEmpty()){
+                Snackbar.make(lLayout, "please add food", Snackbar.LENGTH_LONG).show()
+            }else if (edf.text.toString().isNotBlank()) {
+                foods.add(food)
+                edf.text.clear()
+                edf.clearFocus()
 
-            recyclerView.adapter?.notifyDataSetChanged()
+                recyclerView.adapter?.notifyDataSetChanged()
+            }
         }
     }
 }
